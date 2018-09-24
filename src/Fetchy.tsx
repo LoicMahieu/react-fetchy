5b7de2f9c0eea86c0b2ee9297c32a154d8bec5be
Loïc Mahieu <mahieuloic@gmail.com>
2018-09-24 10:11:41 +0200

    Require lodash individual functions

--- src/Fetchy.tsx
+++ src/Fetchy.tsx
@@ -1,4 +1,5 @@
-import { isEqual, pick } from "lodash";
+import isEqual from "lodash/isEqual";
+import pick from "lodash/pick";
 import * as PropTypes from "prop-types";
 import * as React from "react";
 import * as request from "superagent";
